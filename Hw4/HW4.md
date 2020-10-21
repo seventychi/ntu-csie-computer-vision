ec07760a718f8f1f4ab7c16960884eb638999d87
seven <seven.chi@i-chainwin.com>
2020-10-21 13:39:17 +0800

    Homework 4
    Mathematical Morphology - Binary Morphology

---
+++ Hw4/HW4.md
@@ -0,0 +1,190 @@
+# Computer Vision - Homework 4
+
+## 開發環境
+
+- OS: Windows 10 Pro
+
+- Program Language: C# (with .Net Core 3.1)
+- IDE: Visual Studio 2019
+- Project: Console Application
+
+## 程式說明
+
+程式碼主要寫在 Program.cs，各題目程式皆已實作個別方法，由 Main entry 進行呼叫，答案結果儲存於 answers 資料夾。
+
+各題目程式碼片段、參數及相關演算法說明如下：
+
+(A). Dilation: 傳入 Binary Image 跟 Octagon kernel，藉由迴圈取得所有 Pixel 值後，若 Pixel 值為白色，將 Kernel 區域的 Pixel 更新為白色。
+
+```c#
+/// <summary>
+/// 產生 Dilation 影像 (膨脹影像)
+/// </summary>
+/// <param name="srcImg">來源圖片</param>
+/// <param name="kernel">kernel</param>
+/// <returns>Dilation 影像</returns>
+private static Bitmap GetDilationBitmap(Bitmap srcImg, IList<Point> kernel)
+{
+    var result = new Bitmap(srcImg);
+
+    for (var x = 0; x < srcImg.Width; x++)
+    {
+        for (var y = 0; y < srcImg.Height; y++)
+        {
+            // 判斷是否為白色
+            if (srcImg.GetPixel(x, y).R != 255) continue;
+
+            foreach (var point in kernel)
+            {
+                var px = x + point.X;
+                var py = y + point.Y;
+
+                if (px >= 0 && px < srcImg.Width && py >= 0 && py < srcImg.Height)
+                {
+                    result.SetPixel(px, py, Color.White);
+                }
+            }
+        }
+    }
+
+    return result;
+}
+```
+(B). Euosion: 傳入 Binary Image 跟 Octagon kernel，藉由迴圈判斷 Kernel 區域內是否存在且皆為白色，若是則將 Pixel 值更新為白色。
+
+```c#
+/// <summary>
+/// 產生 Erosion 影像 (侵蝕影像)
+/// </summary>
+/// <param name="srcImg">來源影像</param>
+/// <param name="kernel">kernel</param>
+/// <returns>Erosion 影像</returns>
+private static Bitmap GetErosionBitmap(Bitmap srcImg, IList<Point> kernel)
+{
+    var result = CreateBlankBitmap(srcImg.Width, srcImg.Height);
+
+    for (var x = 0; x < srcImg.Width; x++)
+    {
+        for (var y = 0; y < srcImg.Height; y++)
+        {
+            var contained = true;
+
+            foreach (var point in kernel)
+            {
+                var px = x + point.X;
+                var py = y + point.Y;
+
+                if (px < 0 || 
+                    px >= srcImg.Width || 
+                    py < 0 || 
+                    py >= srcImg.Height ||
+                    srcImg.GetPixel(px, py).R != 255)
+                {
+                    contained = false;
+                    break;
+                }
+            }
+
+            if(contained)
+                result.SetPixel(x, y, Color.White);
+        }
+    }
+
+    return result;
+}
+
+```
+
+(C). Opening: 先 Erosion 再 Dilation 後得到 Opening Image
+
+```c#
+/// <summary>
+/// 取得 Open Bitmap (先 Erosion 再 Dilation)
+/// </summary>
+/// <param name="srcImg">來源影像</param>
+/// <param name="kernel">結構</param>
+/// <returns></returns>
+private static Bitmap GetOpenBitmap(Bitmap srcImg, IList<Point> kernel)
+{
+    return GetDilationBitmap(GetErosionBitmap(srcImg, kernel), kernel);
+}
+```
+
+(D). Closing: 先 Dilation 再 Erosion 後得到 Closing Image
+
+```c#
+/// <summary>
+/// 取得 Closing Bitmap (先 Dilation 再 Erosion)
+/// </summary>
+/// <param name="srcImg">來源影像</param>
+/// <param name="kernel">結構</param>
+/// <returns></returns>
+private static Bitmap GetClosingBitmap(Bitmap srcImg, IList<Point> kernel)
+{
+    return GetErosionBitmap(GetDilationBitmap(srcImg, kernel), kernel);
+}
+
+```
+
+(E). Hit-and-miss transform: 建立兩個 Kernel 分別為 L1 及 L2, 將 原影像與 L1 進行 erosion, 原影像的補集與 L2 進行 erosion. 最後再取兩影像的交集
+
+```c#
+private static readonly List<Point> L1 = new List<Point>
+{
+    new Point(0, -1),
+    new Point(0, 0),
+    new Point(1, 0)
+};
+
+private static readonly List<Point> L2 = new List<Point>
+{
+    new Point(1, -1),
+    new Point(2, -1),
+    new Point(1, -2)
+};
+
+/// <summary>
+/// 取得 HitAndMiss Bitmap
+/// </summary>
+/// <param name="srcImg"></param>
+/// <returns></returns>
+private static Bitmap GetHitAndMissBitmap(Bitmap srcImg)
+{
+    var result = CreateBlankBitmap(srcImg.Width, srcImg.Height);
+    var cmpImg = new Bitmap(srcImg);
+    
+    for (var x = 0; x < srcImg.Width; x++)
+    {
+        for (var y = 0; y < srcImg.Height; y++)
+        {
+            cmpImg.SetPixel(x, y, srcImg.GetPixel(x, y).R == 255 ? Color.Black : Color.White);
+        }
+    }
+
+    var e1 = GetErosionBitmap(srcImg, L1);
+    var e2 = GetErosionBitmap(cmpImg, L2);
+
+    for (var x = 0; x < srcImg.Width; x++)
+    {
+        for (var y = 0; y < srcImg.Height; y++)
+        {
+            if (e1.GetPixel(x, y).R == 255 && e2.GetPixel(x, y).R == 255)
+            {
+                result.SetPixel(x, y, Color.White);
+            }
+        }
+    }
+
+    return result;
+}
+```
+
+## 結果圖片
+| (A). <img src="\Hw4\bin\Debug\netcoreapp3.1\answers\a.bmp" alt="a-1" style="zoom:50%;" /> | (B). <img src="\Hw4\bin\Debug\netcoreapp3.1\answers\b.bmp" alt="b-1" style="zoom:50%;" /> | (C). <img src="\Hw4\bin\Debug\netcoreapp3.1\answers\c.bmp" alt="c-1" style="zoom:50%;" /> |
+| ------------------------------------------------------------ | ------------------------------------------------------------ | ------------------------------------------------------------ |
+| (D).<img src="\Hw4\bin\Debug\netcoreapp3.1\answers\d.bmp" alt="a-2" style="zoom:50%;" /> | (E).<img src="\Hw4\bin\Debug\netcoreapp3.1\answers\e.bmp" alt="b-2" style="zoom:50%;" /> |                                                              |
+
+
+
+
+
---
+++ Hw4/Hw4/Program.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+
+namespace Hw4
+{
+    internal class Program
+    {
+        private static readonly List<Point> Octagon = new List<Point>
+        {
+            new Point(-2,-1),
+            new Point(-2, 0),
+            new Point(-2, 1),
+            new Point(-1,-2),
+            new Point(-1,-1),
+            new Point(-1, 0),
+            new Point(-1, 1),
+            new Point(-1, 2),
+            new Point( 0,-2),
+            new Point( 0,-1),
+            new Point( 0, 0),
+            new Point( 0, 1),
+            new Point( 0, 2),
+            new Point( 1,-2),
+            new Point( 1,-1),
+            new Point( 1, 0),
+            new Point( 1, 1),
+            new Point( 1, 2),
+            new Point( 2,-1),
+            new Point( 2, 0),
+            new Point( 2, 1)
+        };
+
+        private static readonly List<Point> L1 = new List<Point>
+        {
+            new Point(0, -1),
+            new Point(0, 0),
+            new Point(1, 0)
+        };
+
+        private static readonly List<Point> L2 = new List<Point>
+        {
+            new Point(1, -1),
+            new Point(2, -1),
+            new Point(1, -2)
+        };
+
+        private static void Main(string[] args)
+        {
+            try
+            {
+                Directory.CreateDirectory("answers");
+
+                using var image = new Bitmap("lena.bmp");
+
+                var binaryBitmap = GetBinaryBitmap(image);
+
+                GetDilationBitmap(binaryBitmap, Octagon)?.Save(@"answers\a.bmp");
+                GetErosionBitmap(binaryBitmap, Octagon)?.Save(@"answers\b.bmp");
+                GetOpenBitmap(binaryBitmap, Octagon)?.Save(@"answers\c.bmp");
+                GetClosingBitmap(binaryBitmap, Octagon)?.Save(@"answers\d.bmp");
+                GetHitAndMissBitmap(binaryBitmap)?.Save(@"answers\e.bmp");
+
+                Console.WriteLine("Histogram Equalization complete...");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// 產生 Dilation 影像 (膨脹影像)
+        /// </summary>
+        /// <param name="srcImg">來源圖片</param>
+        /// <param name="kernel">kernel</param>
+        /// <returns>Dilation 影像</returns>
+        private static Bitmap GetDilationBitmap(Bitmap srcImg, IList<Point> kernel)
+        {
+            var result = new Bitmap(srcImg);
+
+            for (var x = 0; x < srcImg.Width; x++)
+            {
+                for (var y = 0; y < srcImg.Height; y++)
+                {
+                    // 判斷是否為白色
+                    if (srcImg.GetPixel(x, y).R != 255) continue;
+
+                    foreach (var point in kernel)
+                    {
+                        var px = x + point.X;
+                        var py = y + point.Y;
+
+                        if (px >= 0 && px < srcImg.Width && py >= 0 && py < srcImg.Height)
+                        {
+                            result.SetPixel(px, py, Color.White);
+                        }
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// 產生 Erosion 影像 (侵蝕影像)
+        /// </summary>
+        /// <param name="srcImg">來源影像</param>
+        /// <param name="kernel">kernel</param>
+        /// <returns>Erosion 影像</returns>
+        private static Bitmap GetErosionBitmap(Bitmap srcImg, IList<Point> kernel)
+        {
+            var result = CreateBlankBitmap(srcImg.Width, srcImg.Height);
+
+            for (var x = 0; x < srcImg.Width; x++)
+            {
+                for (var y = 0; y < srcImg.Height; y++)
+                {
+                    var contained = true;
+
+                    foreach (var point in kernel)
+                    {
+                        var px = x + point.X;
+                        var py = y + point.Y;
+
+                        if (px < 0 || 
+                            px >= srcImg.Width || 
+                            py < 0 || 
+                            py >= srcImg.Height ||
+                            srcImg.GetPixel(px, py).R != 255)
+                        {
+                            contained = false;
+                            break;
+                        }
+                    }
+
+                    if(contained)
+                        result.SetPixel(x, y, Color.White);
+                }
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// 取得 Open Bitmap (先 Erosion 再 Dilation)
+        /// </summary>
+        /// <param name="srcImg">來源影像</param>
+        /// <param name="kernel">結構</param>
+        /// <returns></returns>
+        private static Bitmap GetOpenBitmap(Bitmap srcImg, IList<Point> kernel)
+        {
+            return GetDilationBitmap(GetErosionBitmap(srcImg, kernel), kernel);
+        }
+
+        /// <summary>
+        /// 取得 Closing Bitmap (先 Dilation 再 Erosion)
+        /// </summary>
+        /// <param name="srcImg">來源影像</param>
+        /// <param name="kernel">結構</param>
+        /// <returns></returns>
+        private static Bitmap GetClosingBitmap(Bitmap srcImg, IList<Point> kernel)
+        {
+            return GetErosionBitmap(GetDilationBitmap(srcImg, kernel), kernel);
+        }
+
+        /// <summary>
+        /// 取得 HitAndMiss Bitmap
+        /// </summary>
+        /// <param name="srcImg"></param>
+        /// <returns></returns>
+        private static Bitmap GetHitAndMissBitmap(Bitmap srcImg)
+        {
+            var result = CreateBlankBitmap(srcImg.Width, srcImg.Height);
+            var cmpImg = new Bitmap(srcImg);
+            
+            for (var x = 0; x < srcImg.Width; x++)
+            {
+                for (var y = 0; y < srcImg.Height; y++)
+                {
+                    cmpImg.SetPixel(x, y, srcImg.GetPixel(x, y).R == 255 ? Color.Black : Color.White);
+                }
+            }
+
+            var e1 = GetErosionBitmap(srcImg, L1);
+            var e2 = GetErosionBitmap(cmpImg, L2);
+
+            for (var x = 0; x < srcImg.Width; x++)
+            {
+                for (var y = 0; y < srcImg.Height; y++)
+                {
+                    if (e1.GetPixel(x, y).R == 255 && e2.GetPixel(x, y).R == 255)
+                    {
+                        result.SetPixel(x, y, Color.White);
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// 取得 Binary Image (Threshold at 128)
+        /// </summary>
+        /// <param name="srcImg">來源影像</param>
+        /// <returns>二位元影像</returns>
+        private static Bitmap GetBinaryBitmap(Bitmap srcImg)
+        {
+            if (srcImg == null)
+                throw new Exception("source image is null");
+
+            var dstBmp = new Bitmap(srcImg.Width, srcImg.Height);
+
+            // 兩層迴圈取得所有 pixel 值(x, y)
+            for (var x = 0; x < srcImg.Width; x++)
+            for (var y = 0; y < srcImg.Height; y++)
+            {
+                // 取得 Pixel RGB 值, 除以 3 後得到灰階值
+                var color = srcImg.GetPixel(x, y);
+                var gray = (color.R + color.G + color.B) / 3;
+
+                // 值 >= 128 轉為白色(255, 255, 255), 否則轉為黑色(0, 0, 0)
+                dstBmp.SetPixel(x, y, gray >= 128 ? Color.White : Color.Black);
+            }
+
+            return dstBmp;
+        }
+
+        /// <summary>
+        /// 建立黑底的圖片
+        /// </summary>
+        /// <param name="width"></param>
+        /// <param name="height"></param>
+        /// <returns></returns>
+        private static Bitmap CreateBlankBitmap(int width, int height)
+        {
+            var img = new Bitmap(width, height);
+
+            for (var x = 0; x < width; x++)
+            {
+                for (var y = 0; y < height; y++)
+                {
+                    img.SetPixel(x, y, Color.Black);
+                }
+            }
+
+            return img;
+        }
+    }
+}
